104eba7972b4df195d326fed1785ce2efeb11b49
Brooke Mortensen <brookeamymo@gmail.com>
2018-02-04 16:56:48 -0700

    added place to put image after guessing word

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,23 +9,46 @@ var words = [
     "LukeSkywalker",
     "PrincessLeia"
 ];
-var userGuess;
+var winCount = 0;  
+var lettersGuessed = [];                   
+var guessesRemaining = 10;
 
 function resetGame() {
     // Select a random word
     var newWord = words[Math.floor(Math.random() * words.length)];
-    document.querySelector("#currentWord").innerHTML = newWord;
+    // Change the letters into underscores
+		var underscoreArray = []
+		for (var i = 0 ; i < newWord.length; i++) {
+			underscoreArray.push("_");
+		}
+        underscoredWord = underscoreArray;
+    // Make the ',' into spaces
+    // Display the unsolved word
+    document.querySelector("#currentWord").innerHTML = underscoredWord;
     // put guesses remaining = 10;
     document.querySelector("#remainingGuesses").innerHTML = 10;
-// make letters guessed a blank array;
-// Put it in a variable
+    // make letters guessed a blank array;
+    var lettersGuessed = [];
 };
 resetGame ();
-function createGame() {
-// Change the letters into underscores, with spaces
-// Put it in id=currentWord
-};
-/*document.querySelector('#currentWord').innerHTML = 'test';*/
+
+function gamePlay (){
+    //when user presses a key
+        //that key is placed into the lettersGuessed Array
+        document.onkeyup = function (event) {
+            userGuess = event.key;
+            lettersGuessed.push(userGuess);
+        }
+        document.querySelector("#guessedLetters").innerHTML = lettersGuessed;
+        
+        //if the letter was in the word, replace the _ with the letter
+        //if the letter was not in the word, subtract 1 from the remainingGuesses
+
+        //once the remainingGuesses = 0, GAME OVER
+        //once to word is completed, display picture and song
+}
+gamePlay ();
+//resetGame ();
 
 
 
