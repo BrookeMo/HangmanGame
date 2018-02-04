5b9505bf72d4e269f4770ad537400b08957918a4
Brooke Mortensen <brookeamymo@gmail.com>
2018-02-04 15:13:48 -0700

    Javascript progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,3 +9,27 @@ var words = [
     "LukeSkywalker",
     "PrincessLeia"
 ];
+var userGuess;
+
+function resetGame() {
+    // Select a random word
+    var newWord = words[Math.floor(Math.random() * words.length)];
+    document.querySelector("#currentWord").innerHTML = newWord;
+    // put guesses remaining = 10;
+    document.querySelector("#remainingGuesses").innerHTML = 10;
+// make letters guessed a blank array;
+// Put it in a variable
+};
+resetGame ();
+function createGame() {
+// Change the letters into underscores, with spaces
+// Put it in id=currentWord
+};
+/*document.querySelector('#currentWord').innerHTML = 'test';*/
+
+
+
+document.onkeyup = function (event) {
+    console.log(event.key);
+    userGuess = event.key;
+}
