71afc4d99f46a52d3fb541fa871c97f83ceb9994
Brooke Mortensen <brookeamymo@gmail.com>
2018-02-03 23:18:50 -0700

    Javascript Words

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+// My array of words
+var words = [
+    "QuiGonJinn",
+    "HanSolo",
+    "KyloRen",
+    "PoeDameron",
+    "DarthVader",
+    "JarJarBinks",
+    "LukeSkywalker",
+    "PrincessLeia"
+];
